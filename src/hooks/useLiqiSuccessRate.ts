e1fcffc8f59527ccbb2241b95a64cc55e8918457
iwamoto65 <kosuke00940@icloud.com>
2023-04-12 22:23:35 +0900

    fix liqi success rate

--- src/hooks/useLiqiSuccessRate.ts
+++ src/hooks/useLiqiSuccessRate.ts
@@ -3,5 +3,5 @@ import { fixFloatNumber } from "@/utils/fixFloatNumber"
 export const CulcLiqiSuccessRate = (hule: number, liqi: number) => {
   if (hule === 0) return 0
 
-  return fixFloatNumber(liqi / hule * 100)
+  return fixFloatNumber(hule / liqi * 100)
 }
