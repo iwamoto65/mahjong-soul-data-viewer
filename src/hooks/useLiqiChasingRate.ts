947e8839f63a349ed5a116352f266a9c2b0bd757
iwamoto65 <kosuke00940@icloud.com>
2023-04-21 17:36:43 +0900

    fix liqi preemption rate and chasing rate

--- src/hooks/useLiqiChasingRate.ts
+++ src/hooks/useLiqiChasingRate.ts
@@ -3,5 +3,5 @@ import { fixFloatNumber } from "@/utils/fixFloatNumber"
 export const CulcLiqiChasingRate = (liqi: number, preemption: number) => {
   if (liqi === 0) return 0
 
-  return fixFloatNumber(100 - (liqi / preemption * 100))
+  return fixFloatNumber(100 - (preemption / liqi * 100))
 }
--- src/hooks/useLiqiPreemptionRate.ts
+++ src/hooks/useLiqiPreemptionRate.ts
@@ -3,5 +3,5 @@ import { fixFloatNumber } from "@/utils/fixFloatNumber"
 export const CulcLiqiPreemptionRate = (liqi: number, preemption: number) => {
   if (liqi === 0) return 0
 
-  return fixFloatNumber((liqi / preemption) * 100)
+  return fixFloatNumber((preemption / liqi) * 100)
 }
