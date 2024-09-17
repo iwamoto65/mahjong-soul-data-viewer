c853c7e4f91dbdd7f3baac1d7746c8952bd244bd
iwamoto65 <kosuke00940@icloud.com>
2024-09-18 00:03:06 +0900

    set locale and options to unixTimeConverter func

--- src/features/gameConfig/unixTimeConverter.ts
+++ src/features/gameConfig/unixTimeConverter.ts
@@ -1,6 +1,8 @@
 export const convertUnixTime = (unixtime: number): string => {
+  const locale = 'ja-JP'
+  const options = { timeZone: 'Asia/Tokyo' }
   const datetime: Date = new Date(unixtime * 1000)
-  const jpTime: string = datetime.toLocaleDateString() + '/' + datetime.toLocaleTimeString()
+  const jpTime: string = datetime.toLocaleDateString(locale, options) + '/' + datetime.toLocaleTimeString(locale, options)
 
   return jpTime
 }
