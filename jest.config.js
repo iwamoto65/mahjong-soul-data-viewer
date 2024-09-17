6313dfc3d63cd164406da7dab40b3b0b4dc41099
iwamoto65 <kosuke00940@icloud.com>
2024-09-17 18:50:51 +0900

    build test deveopment

---
+++ jest.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jest-environment-jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  moduleDirectories: ['node_modules', '<rootDir>/'],
+};
