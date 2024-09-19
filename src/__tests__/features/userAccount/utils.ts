317b0fdda5413858cd6a55cc2a942954e55c8367
iwamoto65 <kosuke00940@icloud.com>
2024-09-19 15:34:36 +0900

    fix object property

--- src/__tests__/features/userAccount/utils.ts
+++ src/__tests__/features/userAccount/utils.ts
@@ -3,7 +3,7 @@ import type { SeatIndex } from '@/types/playerData';
 import type { UserAccount } from '@/types/userAccount';
 
 export const initializeMockUserAccounts = (length: number, userAccountAttr?: Partial<UserAccount>): UserAccount[] => {
-  const arr = Array.from({ length: length }, (_, i) => i)
+  const arr = Array.from({ length }, (_, i) => i)
   const mockUserAccounts = arr.map((num) => createMockUserAccount({ nickname: `test${num}`, seat: num as SeatIndex, ...userAccountAttr }))
 
   return mockUserAccounts
