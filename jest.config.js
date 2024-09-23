16903ceb758db09aa711114e18e6843466cf2acc
iwamoto65 <kosuke00940@icloud.com>
2024-09-23 22:44:22 +0900

    prevent modification of tsconfig.json

--- jest.config.js
+++ jest.config.js
@@ -3,5 +3,11 @@ module.exports = {
   testEnvironment: 'jest-environment-jsdom',
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
   moduleDirectories: ['node_modules', '<rootDir>/'],
-  testPathIgnorePatterns: ['/__tests__/.*utils\\.ts$']
+  testPathIgnorePatterns: ['/__tests__/.*utils\\.ts$'],
+  transform: {
+    '^.+\\.tsx?$': ['ts-jest', {
+      tsconfig: '<rootDir>/tsconfig.json',
+      isolatedModules: true,
+    }],
+  },
 };
