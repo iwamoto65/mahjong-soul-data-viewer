e98c1d7758d2f165bad177c6959772cdd319c3d5
iwamoto65 <kosuke00940@icloud.com>
2025-01-12 22:29:48 +0900

    change redirect target

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,15 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server';
 
-const allowedBasePath = '/game/latest/demo';
-
 export function middleware(request: NextRequest) {
-  const { pathname } = request.nextUrl;
-
-  if (pathname === allowedBasePath || pathname === allowedBasePath + '/' || pathname.startsWith(allowedBasePath + '/')) {
-    return NextResponse.next();
-  }
-
-  return NextResponse.redirect(new URL('/game/latest/demo', request.url));
+  return NextResponse.next();
 }
 
 export const config = {
